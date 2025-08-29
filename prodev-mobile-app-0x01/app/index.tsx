8f9c0e8526d74e64cc34c1c75c5a78edb9fc63ba
Hillary Robert <roberthillary30@gmail.com>
2025-08-29 15:33:28 +0300

    Implementing Mobile Components in React Native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.largeText}>
