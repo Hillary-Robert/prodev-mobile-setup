3dc914186e919e644dded673009f172267449a28
Hillary Robert <roberthillary30@gmail.com>
2025-08-29 18:50:10 +0300

    Create Your First Mobile App

--- prodev-mobile-app-0x00/app/_layout.tsx
+++ prodev-mobile-app-0x00/app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
-export default function Layout() {
-  return <Tabs />;
+export default function RootLayout() {
+  return <Stack />;
 }
