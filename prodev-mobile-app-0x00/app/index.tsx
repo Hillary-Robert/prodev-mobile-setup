588fd23ee7ef5d5802f94e38ba181344dcfa1933
Hillary Robert <roberthillary30@gmail.com>
2025-08-29 18:57:46 +0300

    Create Your First Mobile App

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
 
-export default function Index() {
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>First App Created</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
